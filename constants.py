1f0ff37c6d9ebc01b2097e10c3165e672416d97c
orelogo <orelogo@gmail.com>
2021-03-21 09:29:34 -0700

    Add postgres database and air quality table

---
+++ constants.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+from collections import namedtuple
+
+TIMESTAMP = "timestamp"
+
+# PM 1.0 concentration in μg/m^3, corrected to standard atmophere conditions
+PM1_STANDARD = "pm1_standard"
+# PM 2.5 concentration in μg/m^3, corrected to standard atmophere conditions
+PM25_STANDARD = "pm25_standard"
+# PM 10 concentration in μg/m^3, corrected to standard atmophere conditions
+PM10_STANDARD = "pm10_standard"
+# PM 10 concentration in μg/m^3, in the current ambient conditions
+PM1_AMBIENT = "pm1_ambient"
+# PM 10 concentration in μg/m^3, in the current ambient conditions
+PM25_AMBIENT = "pm25_ambient"
+# PM 10 concentration in μg/m^3, in the current ambient conditions
+PM10_AMBIENT = "pm10_ambient"
+# number of particles with diameter >0.3 μm in 0.1 L (0.0001 m^3) of air
+PARTICLES_03 = "particles_03"
+# number of particles with diameter >0.5 μm in 0.1 L (0.0001 m^3) of air
+PARTICLES_05 = "particles_05"
+# number of particles with diameter >1.0 μm in 0.1 L (0.0001 m^3) of air
+PARTICLES_1 = "particles_1"
+# number of particles with diameter >2.5 μm in 0.1 L (0.0001 m^3) of air
+PARTICLES_25 = "particles_25"
+# number of particles with diameter >5.0 μm in 0.1 L (0.0001 m^3) of air
+PARTICLES_5 = "particles_5"
+# number of particles with diameter >10.0 μm in 0.1 L (0.0001 m^3) of air
+PARTICLES_10 = "particles_10"
+
+AirQuality = namedtuple("AirQuality", [
+    TIMESTAMP,
+    PM1_STANDARD,
+    PM25_STANDARD,
+    PM10_STANDARD,
+    PM1_AMBIENT,
+    PM25_AMBIENT,
+    PM10_AMBIENT,
+    PARTICLES_03,
+    PARTICLES_05,
+    PARTICLES_1,
+    PARTICLES_25,
+    PARTICLES_5,
+    PARTICLES_10,
+])
--- pms5003.py
+++ pms5003.py
@@ -1,28 +1,11 @@
+#!/usr/bin/env python3
+
 import serial
 import datetime
-from collections import namedtuple
-from os import path
 
-AirQuality = namedtuple('AirQuality', [
-    'pm1_standard',   # PM 1.0 concentration in μg/m^3, corrected to standard atmophere conditions
-    'pm25_standard',  # PM 2.5 concentration in μg/m^3, corrected to standard atmophere conditions
-    'pm10_standard',  # PM 10 concentration in μg/m^3, corrected to standard atmophere conditions
-    'pm1_ambient',    # PM 10 concentration in μg/m^3, in the current ambient conditions
-    'pm25_ambient',   # PM 10 concentration in μg/m^3, in the current ambient conditions
-    'pm10_ambient',   # PM 10 concentration in μg/m^3, in the current ambient conditions
-    # number of particles with diameter >0.3 μm in 0.1 L (0.0001 m^3) of air
-    'particles_03',
-    # number of particles with diameter >0.5 μm in 0.1 L (0.0001 m^3) of air
-    'particles_05',
-    # number of particles with diameter >1.0 μm in 0.1 L (0.0001 m^3) of air
-    'particles_1',
-    # number of particles with diameter >2.5 μm in 0.1 L (0.0001 m^3) of air
-    'particles_25',
-    # number of particles with diameter >5.0 μm in 0.1 L (0.0001 m^3) of air
-    'particles_5',
-    # number of particles with diameter >10.0 μm in 0.1 L (0.0001 m^3) of air
-    'particles_10',
-])
+from os import path
+import constants
+import db
 
 BYTE_COUNT = 32
 FIRST_BYTE = 0x42
@@ -47,40 +30,46 @@ def verify_data(data: bytes) -> bool:
             or check_sum_calculated != check_sum_expected):
         print("Unexpected data received from sensor")
         return False
-    
+
     return True
 
 
-serial_port=serial.Serial("/dev/ttyAMA0", baudrate = 9600, timeout = 3.0)
-abs_log_path=path.join(path.dirname(__file__), "pms5003_log.txt")
-log=open(abs_log_path, "a+")
+serial_port = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=3.0)
+abs_log_path = path.join(path.dirname(__file__), "pms5003_log.txt")
+log = open(abs_log_path, "a+")
+db.open()
 
 try:
     while True:
-        data=serial_port.read(BYTE_COUNT)
+        data = serial_port.read(BYTE_COUNT)
         if not verify_data(data):
             print("Terminating connection with sensor")
             break
 
-        air_quality=AirQuality(
-            pm1_standard = int.from_bytes(data[4:6], byteorder='big'),
-            pm25_standard = int.from_bytes(data[6:8], byteorder='big'),
-            pm10_standard = int.from_bytes(data[8:10], byteorder='big'),
-            pm1_ambient = int.from_bytes(data[10:12], byteorder='big'),
-            pm25_ambient = int.from_bytes(data[12:14], byteorder='big'),
-            pm10_ambient = int.from_bytes(data[14:16], byteorder='big'),
-            particles_03 = int.from_bytes(data[16:18], byteorder='big'),
-            particles_05 = int.from_bytes(data[18:20], byteorder='big'),
-            particles_1 = int.from_bytes(data[20:22], byteorder='big'),
-            particles_25 = int.from_bytes(data[22:24], byteorder='big'),
-            particles_5 = int.from_bytes(data[24:26], byteorder='big'),
-            particles_10 = int.from_bytes(data[26:28], byteorder='big'),
+        air_quality = constants.AirQuality(
+            **{
+                constants.TIMESTAMP: datetime.datetime.now(),
+                constants.PM1_STANDARD: int.from_bytes(data[4:6], byteorder='big'),
+                constants.PM25_STANDARD: int.from_bytes(data[6:8], byteorder='big'),
+                constants.PM10_STANDARD: int.from_bytes(data[8:10], byteorder='big'),
+                constants.PM1_AMBIENT: int.from_bytes(data[10:12], byteorder='big'),
+                constants.PM25_AMBIENT: int.from_bytes(data[12:14], byteorder='big'),
+                constants.PM10_AMBIENT: int.from_bytes(data[14:16], byteorder='big'),
+                constants.PARTICLES_03: int.from_bytes(data[16:18], byteorder='big'),
+                constants.PARTICLES_05: int.from_bytes(data[18:20], byteorder='big'),
+                constants.PARTICLES_1: int.from_bytes(data[20:22], byteorder='big'),
+                constants.PARTICLES_25: int.from_bytes(data[22:24], byteorder='big'),
+                constants.PARTICLES_5: int.from_bytes(data[24:26], byteorder='big'),
+                constants.PARTICLES_10: int.from_bytes(data[26:28], byteorder='big'),
+            }
         )
 
-        log.write(f"{str(datetime.datetime.now())} - {air_quality} \n")
+        db.insert(air_quality)
+        log.write(str(air_quality))
         log.flush()
-        print(f"{str(datetime.datetime.now())} - {air_quality}")
+        print(str(air_quality))
 
 finally:
-    print("Closing program")
+    db.close()
     log.close()
+    print("Closing program")
