527cb49813cd7d9ee24d4192c354a92c1e7413bb
orelogo <orelogo@gmail.com>
2021-03-28 13:05:28 -0700

    Use absolute path for log file

--- logger.py
+++ logger.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 import logging
+from os import path
 
 LOGGER_NAME = 'fartberry'
-LOG_FILENAME = 'fartberry.log'
 LOG_FORMAT = '[%(asctime)s][%(name)s][%(levelname)s] %(message)s'
 
 logger = logging.getLogger(LOGGER_NAME)
@@ -14,7 +14,8 @@ print_handler.setFormatter(print_formatter)
 print_handler.setLevel(logging.INFO)
 logger.addHandler(print_handler)
 
-file_handler = logging.FileHandler(LOG_FILENAME)
+abs_log_path = path.join(path.dirname(__file__), 'fartberry.log')
+file_handler = logging.FileHandler(abs_log_path)
 file_formatter = logging.Formatter(LOG_FORMAT)
 file_handler.setFormatter(file_formatter)
 file_handler.setLevel(logging.DEBUG)
